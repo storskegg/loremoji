31bb8c0d450a181ee698691b1dd45b7f7fd458bf
Liam Conrad <liam@storskegg.org>
2023-05-12 09:11:34 -0500

    a bit further

--- internal/emojiscrape/emojiscrape.go
+++ internal/emojiscrape/emojiscrape.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"sort"
@@ -11,13 +12,19 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
+// maxBodySize is set to an unreasonably large number, because the file size as of this moment is 33.83M, and will
+// likely only grow as more BS is added to the emoji codepages of the unicode standard. (What a waste.)
+const maxBodySize = 128 * 1024 * 1024 // 128MB
 const unicodeEmojiURL = "https://www.unicode.org/emoji/charts/full-emoji-list.html"
 const codeSelector = "td.code"
+
+var pkgName string
+var out string
 
 var exitCode = 0
 
 type collector struct {
-	codes map[int]struct{}
+	codes map[int]struct{} // a map is used to dedupe
 	mu    sync.Mutex
 }
 
@@ -57,25 +64,30 @@ func main() {
 		os.Exit(exitCode)
 	}()
 
+	flag.StringVar(&pkgName, "pkg", "", "target package name")
+	flag.StringVar(&out, "out", "", "output file name")
+	flag.Parse()
+
 	cc := NewCodesCollector()
 
-	c := colly.NewCollector(colly.AllowURLRevisit())
+	c := colly.NewCollector(colly.AllowURLRevisit(), colly.MaxBodySize(maxBodySize))
 
 	count := 0
 
 	c.OnHTML(codeSelector, func(e *colly.HTMLElement) {
 		s := strings.Split(e.Text, " ")[0]
 		s = strings.ReplaceAll(s, "U+", "")
+
 		i, err := strconv.ParseInt(s, 16, 32)
 		if err != nil {
 			fmt.Println(err)
 			exitCode = 1
 			return
 		}
+
 		if i > 100000 {
 			count++
 			cc.Push(int(i))
-			fmt.Printf("[%d] %d\n", count, int(i))
 		}
 	})
 
@@ -98,6 +110,9 @@ func main() {
 
 	fmt.Println("Iterated  : ", count)
 	fmt.Println("Collected : ", cc.Size())
+
+	sb := strings.Builder{}
+	fmt.Fprintln(&sb, "")
 }
 
 /**
@@ -105,3 +120,15 @@ func main() {
  *
  * [...document.querySelectorAll('.code')].map((v, k) => parseInt(v.innerText.split(' ')[0].replace('U+', '0x'), 16)).filter(a => a > 100000).sort()
  */
+
+func validatePackageName(name string) bool {
+	if name == "" {
+		return false
+	}
+
+	if strings.ContainsAny(name, ".-") {
+		return false
+	}
+
+	return true
+}
